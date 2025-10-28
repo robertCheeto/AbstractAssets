46b68090dc2379ed48b180dbdaac6829c84cb6e1
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-28 13:59:49 -0400

    * updated Main values for testing

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -8,11 +8,11 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Asset> assets = new ArrayList<>();
 
-        House home1 = new House("My First Home", "May 23, 2023",115935.18, "1618 Poolside Lane, Charlotte, NC 28208", 4, 1600, 5000);
+        House home1 = new House("My First Home", "May 23, 2023",115935.18, "1618 Poolside Lane, Charlotte, NC", 4, 1600, 5000);
         House home2 = new House("Cabin Home", "April 23, 1998", 5000.88, "100 Roger Hideaway, Champion, PA", 2, 10000, 20000);
 
-        Vehicle car1 = new Vehicle("First Car", "November 30, 2022", 23500.00, "Hyundai Elantra", 2023, 156723);
-        Vehicle car2 = new Vehicle("Big Ole Truck", "June 3, 1999", 23500.00, "Toyota Tacoma", 1998, 156723);
+        Vehicle car1 = new Vehicle("First Car", "November 30, 2022", 23500.00, "Hyundai Elantra", 2023, 15225);
+        Vehicle car2 = new Vehicle("Big Ole Truck", "June 3, 1999", 1895.00, "Toyota Tacoma", 1998, 256723);
 
         assets.add(home1);
         assets.add(home2);
